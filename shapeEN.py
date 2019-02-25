feef593eb6e284395831d955ba790fc1e6e174e9
eloge ndja <eloge.ndja@uamaker.nyc>
2019-02-25 10:51:25 -0500

    finished the circle

--- shapeEN.py
+++ shapeEN.py
@@ -1,14 +1,19 @@
 from graphics import *
+
 shapesWin = GraphWin ("shapes", 500, 500)
 shapesWin.setCoords ( 0,0,500,500)
 
-rTri = Polygon (Point (25, 25), Point (75,125), Point (125, 25))
+rTri = Polygon (Point (10, 10), Point (60,110), Point (110, 10))
 rTri.setFill (color_rgb(225,0,0))
 rTri.draw(shapesWin)
 
 gOval = Oval (Point (490, 490), Point(450,375))
 gOval.setFill (color_rgb(0,225,0))
 gOval.draw(shapesWin)
+
+bCir = Circle (Point(75,440), 50)
+bCir.setFill(color_rgb(0,0,225))
+bCir.draw(shapesWin)
 
 shapesWin.getMouse ()
 shapesWin.close()
